1c4adb069cbba03864d1936108040f49632a0f1e
QuocBinh042 <tranlequocbinh2003@gmail.com>
2024-10-09 12:21:38 +0700

    Update

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import Screen1 from './screens/Screen1'
 import Screen2 from './screens/Screen2'
 import Screen3 from './screens/Screen3'
 import Screen4 from './screens/Screen4'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
